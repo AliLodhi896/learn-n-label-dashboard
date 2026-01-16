4f1e8cfcb6f464164fa987cabe9af64eeeba647a
Ali Lodhi <103258113+AliLodhi896@users.noreply.github.com>
2026-01-16 16:37:37 +0500

    types error fixed

--- src/helpers/capitalize-pathname.ts
+++ src/helpers/capitalize-pathname.ts
@@ -4,7 +4,7 @@ function capitalizePathname(input: string): string {
   if (lastSegment) {
     return lastSegment
       .split('-')
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
       .join(' ');
   } else return '';
 }
